9447e3840fbc1a45736adbc13f628c5d39d05781
Brian Won <brianwontonsoup@gmail.com>
2015-03-10 04:03:36 -0400

    Quickselect works, but not quicksort.

--- 06quickselect/select.java
+++ 06quickselect/select.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class select{
 
-    public static int partition(int[] hi,int si, int ei,int n){
+    public static int quickselect(int[] hi,int si, int ei,int n){
 	int a=si;
 	int b=ei;
 	if (si==ei){
@@ -33,15 +33,104 @@ public class select{
 	if (si==n){
 	    return d[si];
 	}else if (si<n){
-	    return partition(d,si,b,n);
+	    return quickselect(d,si,b,n);
 	}else if (si>n){
-	    return partition(d,a,ei,n);
+	    return quickselect(d,a,ei,n);
 	}
 	return -1;
 	    
 	//show(d);
 	//System.out.println(hi[pivot]);
     }
+
+    public static int quickselect2(int[]ary,int si,int ei,int n){
+	int a=si;
+	int b=ei;
+	Random rand = new Random();
+	int pivot = rand.nextInt(ei-si+1)+si;
+	int swap = ary[ei+1];
+	ary[ei+1]=ary[pivot];
+	ary[pivot]=swap;
+	int pivot2=ei+1;
+	boolean keepgoing=true;
+	while(keepgoing){
+	    while (si<ei && ary[si]<ary[pivot2]){
+		si++;
+	    }
+	    while (ei>si && ary[ei]>ary[pivot2]){
+		ei++;
+	    }
+	    if (si==ei){
+		swap=ary[si];
+		ary[si]=ary[pivot2];
+		ary[pivot2]=swap;
+		keepgoing=false;
+	    }else{
+		swap=ary[ei];
+		ary[ei]=ary[si];
+		ary[si]=swap;
+	    }
+	}
+	if (si<n){
+	    return quickselect2(ary,si+1,b,n);
+	}else if(si>n){
+	    return quickselect2(ary,a,si-1,n);
+	}else{
+	    show(ary);
+	    return ary[si];
+	}
+	
+	
+    }
+
+    public static void quicksort(int[]ary){
+    	quickH(ary,0,ary.length-2);
+	show(ary);
+    }
+
+    public static void quickH(int[]ary,int si,int ei){
+    	boolean keepgoing=true;
+    	while(keepgoing){
+    	    if (si==ei){
+    		keepgoing=false;
+    	    }else{
+		int a=si;
+		int b=ei;
+		System.out.println(si);
+		System.out.println(ei);
+    		Random rand = new Random();
+    		int pivot = rand.nextInt(ei-si+1)+si;
+    		int swap = ary[ei+1];
+    		ary[ei+1]=ary[pivot];
+    		ary[pivot]=swap;
+		int pivot2=ei+1;
+		boolean keepgoing2=true;
+		while(keepgoing2){
+		    while (si<ei && ary[si]<ary[pivot2]){
+			si++;
+		    }
+		    while (ei>si && ary[ei]>ary[pivot2]){
+			ei++;
+		    }
+		    if (si==ei){
+			swap=ary[si];
+			ary[si]=ary[pivot2];
+			ary[pivot2]=swap;
+			keepgoing2=false;
+		    }else{
+			swap=ary[ei];
+			ary[ei]=ary[si];
+			ary[si]=swap;
+		    }
+		}
+		quickH(ary,si+1,b+1);
+		quickH(ary,a,si-1);
+	    }
+	}
+    }
+	    
+		
+		
 
    
     public static void show(int[]yo){
@@ -67,6 +156,6 @@ public class select{
 	sam[7] = 9;
 	sam[8] = 0;
 	sam[9] = 8;
-	System.out.println(select.partition(sam,0,9,9));
+	select.quicksort(sam);
     }
 }
