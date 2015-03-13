8cb3fd55a3659bf8351f15c6c1bf17f645d5f4aa
Brian Won <brianwontonsoup@gmail.com>
2015-03-13 12:19:26 -0400

    Not done yet.

--- 07linkedlist/MyLinkedList.java
+++ 07linkedlist/MyLinkedList.java
@@ -13,4 +13,31 @@ public class MyLinkedList{
 	}
 	return list.substring(0,list.length()-1)+" ]";
     }
+
+    public int get(int index){
+	if (index<0 || index>=size()){
+	    throw new IndexOutOfBoundsException();
+	}else{
+	    LNode out=head;
+	    for (int i=0;i<size();i++){
+		out=out.getNext();
+	    }
+	    return out;
+	}
+    }
+    
+    public int set(int index,int value){
+	if (index<0 || index>=size()){
+	    throw new IndexOutOfBoundsException();
+	}else{
+	    LNode temp=head;
+	    for (int i=0;i<size();i++){
+		temp=temp.getNext();
+	    }
+	    int out=temp.getValue();
+	    temp.setValue(value);
+	    return out;
+	}
+    }
+	
 }
