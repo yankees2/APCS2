4f67aec204a33502d8ed23b4baa6c4e0f85879f3
Brian Won <brianwontonsoup@gmail.com>
2015-03-30 02:19:40 -0400

    Finished

--- 010DQ/MyDeque.java
+++ 010DQ/MyDeque.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class MyDeque<T>{
     private Object[] things;
@@ -6,16 +6,20 @@ public class MyDeque<T>{
 
     public MyDeque(){
 	things=new Object[10];
+	size=0;
     }
 
     public void addFirst(T value){
+	if (size()==things.length){
+	    resize();
+	}
 	if (size()==0){
 	    things[0]=(Object)value;
 	    head=0;
 	    tail=0;
 	}else if(head==0){
-	    things[size()-1]=(Object)value;
-	    head=size()-1;
+	    things[things.length-1]=(Object)value;
+	    head=things.length-1;
 	}else{
 	    things[head-1]=(Object)value;
 	    head--;
@@ -24,6 +28,9 @@ public class MyDeque<T>{
     }
 
     public void addLast(T value){
+	if (size()==things.length){
+	    resize();
+	}
 	if (size()==0){
 	    things[0]=(Object)value;
 	    head=0;
@@ -35,14 +42,102 @@ public class MyDeque<T>{
 	    things[tail+1]=(Object)value;
 	    tail++;
 	}
-	size--;
+	size++;
     }
     
-    public void resize(Object[]hi){
+    public void resize(){
+	Object[] newguy = new Object[things.length*2];
+	int index=0;
+	if(head>tail){
+	    while(head<things.length){
+		newguy[index]=things[head];
+		head++;
+		index++;
+	    }
+	    int x=0;
+	    while(x<=tail){
+		newguy[index]=things[x];
+		x++;
+		index++;
+	    }
+	}else{
+	    while(head<=tail){
+		newguy[index]=things[head];
+		head++;
+		index++;
+	    }
+	}
+	head=0;
+	tail=index-1;
+	things=newguy;		
+    }
+
+    public T removeFirst(){
+	if(size()==0){
+	    throw new NoSuchElementException();
+	} else {
+	    T out=(T)things[head];
+	    things[head]=null;
+	    if(size()==1){
+		head=0;
+		tail=0;
+	    }else if(head==things.length-1){
+		head=0;
+	    }else{
+		head++;
+	    }
+	    size--;
+	    return out;
+	}
+    }
+
+    public T removeLast(){
+	if(size()==0){
+	    throw new NoSuchElementException();
+	}else{
+	    T out=(T)things[tail];
+	    things[tail]=null;
+	    if (size()==1){
+		head=0;
+		tail=0;
+	    }else if(tail==0){
+		tail=things.length-1;
+	    }else{
+		tail--;
+	    }
+	    size--;
+	    return out;
+	}
+    }
+	    
+    public T getFirst(){
+	if(size()==0){
+	    throw new NoSuchElementException();
+	}else{
+	    T out=(T)things[head];
+	    return out;
+	}
+    }
+
+    public T getLast(){
+	if(size()==0){
+	    throw new NoSuchElementException();
+	}else{
+	    T out=(T)things[tail];
+	    return out;
+	}
     }
 
     public int size(){
 	return size;
+    }
+
+    public int head(){
+	return head;
+    }
+
+    public int tail(){
+	return tail;
     }
     
     public String toString(){
@@ -51,8 +146,17 @@ public class MyDeque<T>{
 
     public static void main (String[]args){
 	MyDeque<String> a = new MyDeque<String>();
-	a.addFirst("Hey");
-	a.addLast("boy");
-	System.out.println(a.toString());
+	a.addLast("1");
+	a.addLast("2");
+	a.addLast("3");
+	a.addLast("4");
+	a.addLast("5");
+	a.addLast("6");
+	a.addLast("7");
+	a.addLast("8");
+	a.addLast("9");
+	a.addLast("10");
+	System.out.println(a.removeFirst());
+	System.out.println(a.size());
     }
 }
